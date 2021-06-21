f45f45fcce9c399589b3e58a318f706606262952
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-06-21 19:05:09 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header :navBarData="navBarData"/>
+    <Header :navBarData="navBarData" :phone="phone" :userInfo="userInfo"/>
     <Main />
     <Footer />
   </div>
@@ -47,7 +47,14 @@ export default {
           list : false,
         },
       ],
-
+      phone : "(555) 802-1234",
+      userInfo : [
+        {
+          name : "Richard Madsen",
+          info : "Marketing Consultant Expert",
+          img : "marketing-consultant-expert.jpg"
+        },
+      ]
     }
   }
 }
@@ -63,15 +70,50 @@ export default {
   * {
     font-family: "Roboto";
   }
+  .ms_container {
+    margin: 0 auto;
+    max-width: 1300px;
+    height: 100%;
+  }
+  .space-bet {
+    display: flex;
+    justify-content: space-between;
+  }
+  .align-center {
+    display: flex;
+    align-items: center;
+  }
+  .phone {
+    display: flex;
+    align-items: center;
+    i {
+      margin-right: 20px;
+    }
+  }
   .orange {
     color: $colorOrange;
   }
-  .backLightOrange {
+  .btnLightOrange {
     background-color: rgba($colorOrange, 0.2);
+  }
+  .btnOrange {
+    color: white;
+    background-color: $colorOrange;
+  }
+  .btnWhite {
+    background-color: white;
+    color: black;
   }
   .btn {
     border-radius: 10px;
     padding: 15px 35px;
     font-size: 18px;
+  }
+  .btn-lg {
+    width: 100%;
+    text-align: center;
+    font-size: 18px;
+    border-radius: 10px;
+    padding: 20px 0;
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="header">
         <div id="nav_bar">
-            <div class="ms_container">
+            <div class="ms_container space-bet align-center">
                 <div class="nav_left">
                     <img id="logo" src="../assets/images/avada-marketing-logo.png" alt="logo">
                     <ul>
@@ -14,14 +14,41 @@
                 <div class="nav_right">
                     <div class="phone">
                         <i class="fas fa-phone-alt orange"></i>
-                        <p>(555) 802-1234</p>
+                        <p>{{phone}}</p>
                     </div>
-                    <button class="btn orange backLightOrange">Free Quote</button>
+                    <button class="btn orange btnLightOrange">Free Quote</button>
                 </div>
             </div>
         </div>
         <div id="jumbotron">
-            
+            <div class="veil">
+                <div class="ms_container space-bet">
+                    <div class="j-left">
+                        <h1>Unlock Your Online Growth Potential</h1>
+                        <h3>Online marketing to secure custumers retention, leads, and sales. We focus on the bigger picture.</h3>
+                        <button class="btn btnWhite">
+                            Our Services
+                            <i class="fas fa-arrow-right"></i>
+                        </button>
+                    </div>
+                    <div class="callback">
+                        <img :src="require(`../assets/images/${userInfo[userActive].img}`)" alt="profilo">
+                        <h3>{{userInfo[userActive].name}}</h3>
+                        <h4>{{userInfo[userActive].info}}</h4>
+                        <div class="phone">
+                            <i class="fas fa-phone-alt orange"></i>
+                            <p class="orange">{{phone}}</p>
+                        </div>
+                        <form>
+                            <input type="text" placeholder="Your Name*">
+                            <input type="email" placeholder="Your Email*">
+                            <input type="number" placeholder="Your Phone Number*">
+                        </form>
+                        <button class="btn-lg btnOrange">Get a Callback</button>
+                        <p>By submitting my data I agree to be contacted</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -31,24 +58,96 @@ export default {
     name : "Header",
     props : {
         navBarData : Array,
+        phone : String,
+        userInfo : Array,
+
+    },
+    data : function () {
+        return {
+            userActive : 0,
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~@fortawesome/fontawesome-free/css/all.min.css';
+    #jumbotron {
+        height: 72vh;
+        background-image: url("../assets/images/marketing-intro.jpg");
+        background-size: cover;
+        background-position-x: -110px;
 
+        .veil {
+            height: 100%;
+            background-color: rgba(black, 0.2);
+            padding-top: 5%;
+        }
+        
+    }
+    .callback {
+        background-color: white;
+        width: 37%;
+        height: 620px;
+        margin-right: 7%;
+        border-radius: 15px;
+        box-shadow: 0 15px 40px rgba(black, 0.2);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: space-around;
+        padding: 20px 30px;
+
+        img {
+            border-radius: 50%;
+            width: 80px;
+        }
+
+        h3 {
+            font-size: 25px;
+            font-weight: 420;
+        }
+        h4 {
+            font-size: 20px;
+            font-weight: 400;
+            color: rgb(133, 135, 134);
+        }
+
+        input {
+            border: 1px solid rgba(133, 135, 134, 0.3);
+            border-radius: 10px;
+            width: 100%;
+            padding: 15px 20px;
+            font-size: 18px;
+            margin-bottom: 15px;
+        }
+        p {
+            font-size: 12px;
+            color: rgb(133, 135, 134);
+        }
+    }
+    .j-left {
+        width: 50%;
+        color: white;
+
+        h1 {
+            font-size: 70px;
+            margin-bottom: 40px;
+        }
+        h3 {
+            font-size: 28px;
+            font-weight: 400;
+            margin-bottom: 40px;
+        }
+        button {
+            font-weight: 500;
+            i {
+                margin-left: 5px;
+            }
+        }
+    }
     #nav_bar {
         height: 10vh;
-
-        .ms_container {
-            margin: 0 auto;
-            max-width: 1300px;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-        }
 
         .nav_left {
             display: flex;
@@ -64,13 +163,7 @@ export default {
             }
         }
     }
-    .phone {
-        display: flex;
-        align-items: center;
-        i {
-            margin-right: 20px;
-        }
-    }
+
     ul {
         list-style: none;
         display: flex;
